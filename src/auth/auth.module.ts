adcd57f76352598785e0dc188e7b1f5c1ff044ba
NamMinwu <minwu06255@gmail.com>
2023-05-23 15:50:53 +0900

    CHORE : little fix8

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,13 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { Todo } from 'src/todos/entities/todo.entity';
+import { User } from 'src/users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([Todo, User]),
     UsersModule,
     JwtModule.register({
       global: true,
